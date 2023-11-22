cbe55213b2f6ae29d684d4fe16de5351b9e82d1b
Mohamed Mohsen <144452856+Mohamed1390@users.noreply.github.com>
2023-11-22 09:13:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let typed = new Typed('#element', {
+    strings: [ 'Frontend Web Developer', 
+    'Full Stack Developer', 'Mobile App Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
+  let typed1 = new Typed('#element1', {
+    strings: [ 'Frontend Web Developer', 
+    'Full Stack Developer', 'Mobile App Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+  });
